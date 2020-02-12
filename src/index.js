194a6b363f4a322b44280ff47ad2ec9c634647dc
cholorecto <pocholorecto123@gmail.com>
2020-02-12 22:52:19 +0800

    Added editorconfig and initial modal for adding, updating

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Table from './CelebTable'
 import './App.css'
-import { Row, Col } from 'antd';
+import { Row, Col, Button, Modal } from 'antd';
 
 class Celebrity extends Component {
     constructor(props) {
@@ -11,7 +11,8 @@ class Celebrity extends Component {
         this.state={
             celebs: [],
             selectedData: null,
-            noAlert: true
+            noAlert: true,
+            visible: false,
         }
     }
 
@@ -26,14 +27,44 @@ class Celebrity extends Component {
         this.getData()
     }
 
+    showModal = () => {
+        this.setState({
+          visible: true,
+        });
+      };
+
+      handleOk = e => {
+        this.setState({
+          visible: false,
+        });
+      };
+
+      handleCancel = e => {
+        this.setState({
+          visible: false,
+        });
+      };
+
     render() {
         return (
             <div>
                 <Row>
                     <Col span={12} offset={6}>
+                        <Button type="primary" onClick={this.showModal}>Add</Button>
                         <Table data={this.state.celebs} />
                     </Col>
                 </Row>
+
+                <Modal
+                  title="Celebrity data"
+                  visible={this.state.visible}
+                  onOk={this.handleOk}
+                  onCancel={this.handleCancel}
+                >
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                </Modal>
             </div>
         );
     }
